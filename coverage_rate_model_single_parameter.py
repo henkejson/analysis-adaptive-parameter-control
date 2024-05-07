837c43d97ef46fc41b8e3d844137770a96e6e2b0
Erik <31245545+RedSpettan@users.noreply.github.com>
2024-05-07 15:44:28 +0200

    :hammer: Remade the Bayesian model for Coverage Rate to now use a Normal likelihood

--- coverage_rate_model_single_parameter.py
+++ coverage_rate_model_single_parameter.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-import formulaic
 import pymc as pm
 import arviz as az
+from Notebooks.utils.design_matrix_creator import get_design_matricies
 
 
 
@@ -15,36 +15,19 @@ if __name__ == '__main__':
 
     # Calculate the integral for each row using the trapezoidal rule
     data['IntegralValue'] = coverage_data.apply(lambda row: np.trapz(row, dx=1), axis=1)
-    data['IntegralValue'] = data['IntegralValue'] / 300.0
+                
+    observation_matrix, module_matrix, parameter_matrix, interaction_matrix = get_design_matricies(data, 'IntegralValue')
 
-    # Dummy variables for Module and Parameters
-    model_formula = 'IntegralValue ~ 0 + C(TargetModule) + C(TuningParameters, contr.treatment("NONE"))'
-    design_matrix = formulaic.model_matrix(model_formula, data=data)
-
-    module_matrix = design_matrix.rhs.iloc[:, :24]
-    parameter_matrix = design_matrix.rhs.iloc[:, 24:]
-
-    # Dummy variables for interaction terms
-    model_formula = 'IntegralValue ~ 0 + C(TargetModule) : C(TuningParameters)'
-    design_matrix = formulaic.model_matrix(model_formula, data=data)
-
-    # Filter out columns that contain 'T.NONE' in their name
-    columns_to_drop = [col for col in design_matrix.rhs.columns if 'T.NONE' in col]
-
-    # Drop the identified columns
-    design_matrix.rhs.drop(columns=columns_to_drop, axis=1, inplace=True)
-    interaction_matrix = design_matrix.rhs.iloc[:,:]
-
-    with pm.Model() as model:
+    with pm.Model():
         # Global Intercept and standard deviation for Modules
-        a_bar = pm.Normal('a_bar', mu=0, sigma=1.5)
+        a_bar = pm.Normal('a_bar', mu=150, sigma=30)
         
-        # Standard Deviations for Modules, Parameters and Interactions
-        sigma_a = pm.Exponential('sigma_a', 2.0)
-        sigma_b = pm.Exponential('sigma_b', 5.0)
-        sigma_g = pm.Exponential('sigma_g', 5.0)
+        # Standard Deviations for modules, marameters and nteractions
+        sigma_a = pm.Exponential('sigma_a', 0.03)
+        sigma_b = pm.Exponential('sigma_b', 0.1)
+        sigma_g = pm.Exponential('sigma_g', 0.1)
         
-        # Non-centered parameterizations
+        # Non-centered parameterizations for module, parameter and interaction effect.
         a_offset = pm.Normal('a_offset', mu=0, sigma=1, shape=24)
         a_m = pm.Deterministic('a_m', a_bar + sigma_a * a_offset)
 
@@ -56,19 +39,19 @@ if __name__ == '__main__':
 
         
         # Activate the correct dummy variables
-        logit_a = pm.math.dot(module_matrix, a_m)
-        logit_b = pm.math.dot(parameter_matrix, b_p)
-        logit_g = pm.math.dot(interaction_matrix, g_mp)
+        identity_a = pm.math.dot(module_matrix, a_m)
+        identity_b = pm.math.dot(parameter_matrix, b_p)
+        identity_g = pm.math.dot(interaction_matrix, g_mp)
 
-        # Link function (logit), unbounded to (0,1) probability
-        p = pm.Deterministic('p', pm.math.sigmoid(logit_a + logit_b + logit_g))
+        # Link function (identity)
+        mu = pm.Deterministic('mu', identity_a + identity_b + identity_g)
         
-        # Beta distribution likelihood 
-        theta = pm.Uniform('theta', 10, 200) # Dispersion parameter
-        Y_obs = pm.Beta('Y_obs', alpha=p*theta, beta=(1-p)*theta, observed=design_matrix.lhs['IntegralValue'])
+        # Normal distribution likelihood 
+        sigma = pm.Exponential('sigma', 0.1)
+        Y_obs = pm.Normal('Y_obs', mu=mu, sigma=sigma, observed=observation_matrix['IntegralValue'])
         
         # Sample from the model
-        trace = pm.sample(5000, chains=4, return_inferencedata=True, progressbar=True, target_accept=0.95)
+        trace = pm.sample(5000, chains=4,return_inferencedata=True, progressbar=True, target_accept=0.95)
         log_lik = pm.compute_log_likelihood(trace)
 
     print("Model building complete. Saving results...")
